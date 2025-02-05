dd6614224da5f750f0a1fa88bd06081c7ffef9de
V-Shake <viviencai6918@gmail.com>
2025-02-05 22:46:51 +0100

    Remove PlayQuiz and QuartetGameOverview components to streamline routing

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,6 @@
   import Router from "svelte-spa-router";
   import LandingPage from "./pages/LandingPage.svelte";
   import CardOverview from "./pages/CardOverview.svelte";
-  import QuartetGameOverview from "./pages/QuartetGameOverview.svelte";
-  import PlayQuiz from "./pages/PlayQuiz.svelte";
   import Quiz from "./pages/Quiz.svelte";
   import Navbar from "./components/Navbar.svelte";
   import "../public/fonts/stylesheet.css";
@@ -12,8 +10,6 @@
   const routes = {
     "/": LandingPage,
     "/cards": CardOverview,
-    "/quartet": QuartetGameOverview,
-    "/play-quiz": PlayQuiz,
     "/quiz": Quiz,
   };
 </script>
